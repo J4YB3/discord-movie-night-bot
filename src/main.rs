3f86b9945fc7761ba1aff919e5a37bc512208f7e
Jan <jan-bechtold@web.de>
2021-04-03 01:35:05 +0200

    Added basic functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+extern crate discord_data;
+use discord::{self, Discord as Discord, model as Model};
+
 fn main() {
-    println!("Hello, world!");
+    let bot = Discord::from_bot_token(discord_data::TOKEN).expect("Bot creation from token failed");
+
+    let (mut connection, _ready_event) = bot.connect().expect("Establishing connecting to server failed");
+
+    loop {
+        match connection.recv_event() {
+            Ok(Model::Event::MessageCreate(message)) => {
+                println!("{} says: {}", message.author.name, message.content);
+                if message.content == "!quit" {
+                    let _ = bot.send_message(
+                        message.channel_id,
+                        "Quitting. Bye bye.",
+                        "",
+                        false,
+                    );
+                    break;
+                }
+            },
+            Ok(_) => (),
+            Err(discord::Error::Closed(code, body)) => {
+                println!("Gateway closed on us with code {:?}: {}", code, body);
+            },
+            Err(err) => println!("Received error: {}", err),
+        }
+    }
+
+    let _ = connection.shutdown();
 }
